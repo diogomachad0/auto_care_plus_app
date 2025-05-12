eec54a7f313798b4aae02cbf15e930b9114ace98
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-12 19:47:27 -0300

    feat: implementação da tela de acesso

--- lib/app/modules/autenticacoes/entrar/entrar_screen.dart
+++ lib/app/modules/autenticacoes/entrar/entrar_screen.dart
@@ -1,4 +1,6 @@
 import 'package:auto_care_plus_app/app/shared/mixin/theme_mixin.dart';
+import 'package:auto_care_plus_app/app/shared/widgets/text_field_custom/password_text_field_custom.dart';
+import 'package:auto_care_plus_app/app/shared/widgets/text_field_custom/text_field_custom.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -11,41 +13,140 @@ class EntrarScreen extends StatefulWidget {
 
 class _EntrarScreenState extends State<EntrarScreen> with ThemeMixin {
   @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: colorScheme.onPrimary,
+          ),
+          onPressed: () {
+            Modular.to.navigate('/');
+          },
+        ),
+        title: Text(
+          'Acessar conta',
+          style: textTheme.titleLarge?.copyWith(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      extendBodyBehindAppBar: true,
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
-            colors: [colorScheme.primary, colorScheme.secondary],
+            colors: [
+              colorScheme.primary,
+              colorScheme.secondary,
+            ],
           ),
         ),
         child: SafeArea(
           child: Column(
             children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: Row(
-                  children: [
-                    IconButton(
-                      icon: const Icon(
-                        Icons.arrow_back_ios,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {
-                        Modular.to.navigate('/');
-                      },
+              Expanded(
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(24.0),
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minHeight: MediaQuery.of(context).size.height * 0.8,
                     ),
-                    const SizedBox(width: 62),
-                    Text(
-                      'Acessar conta',
-                      style: textTheme.titleLarge?.copyWith(
-                        color: Colors.white,
+                    child: IntrinsicHeight(
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Card(
+                          elevation: 8,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Image.asset(
+                                  'assets/img/logo_black_app.png',
+                                  width: 200,
+                                ),
+                                const SizedBox(height: 32),
+                                const TextFieldCustom(label: 'E-mail'),
+                                const SizedBox(height: 16),
+                                const PasswordTextFieldCustom(label: 'Senha'),
+                                const SizedBox(height: 24),
+                                FilledButton(
+                                  style: FilledButton.styleFrom(
+                                    minimumSize:
+                                        const Size(double.infinity, 46),
+                                    backgroundColor: colorScheme.primary,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                  onPressed: () {},
+                                  child: Text(
+                                    'Entrar',
+                                    style: textTheme.bodyMedium?.copyWith(
+                                      fontWeight: FontWeight.w500,
+                                      color: colorScheme.onPrimary,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    children: [
+                                      Expanded(
+                                        child: Divider(
+                                          color: Colors.grey.shade300,
+                                          thickness: 1,
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 8.0),
+                                        child: Text(
+                                          'ou',
+                                          style: textTheme.bodyMedium?.copyWith(
+                                            color: Colors.grey.shade300,
+                                          ),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: Divider(
+                                          color: Colors.grey.shade300,
+                                          thickness: 1,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                TextButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    'Esqueci minha senha',
+                                    style: textTheme.bodyMedium?.copyWith(
+                                      color: colorScheme.secondary,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
                       ),
                     ),
-                  ],
+                  ),
                 ),
               ),
             ],
