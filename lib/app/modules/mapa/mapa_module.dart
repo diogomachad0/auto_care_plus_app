aec55d826694072a9a3f1b011d08fbd4fc471e82
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-18 22:48:50 -0300

    feat: estrutura mapa

---
+++ lib/app/modules/mapa/mapa_module.dart
@@ -0,0 +1,12 @@
+import 'package:auto_care_plus_app/app/modules/mapa/mapa_screen.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class MapaModule extends Module {
+  @override
+  void routes(RouteManager r) {
+    r.child(Modular.initialRoute, child: (context) => const MapaScreen());
+
+
+    super.routes(r);
+  }
+}
---
+++ lib/app/modules/mapa/mapa_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MapaScreen extends StatefulWidget {
+  const MapaScreen({super.key});
+
+  @override
+  State<MapaScreen> createState() => _MapaScreenState();
+}
+
+class _MapaScreenState extends State<MapaScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.green,
+      ),
+    );
+  }
+}
