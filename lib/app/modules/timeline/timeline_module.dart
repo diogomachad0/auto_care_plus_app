5b5d80958e86ce0a5287f2a06272d74df75d44eb
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-18 22:48:33 -0300

    feat: estrutura timeline

---
+++ lib/app/modules/timeline/timeline_module.dart
@@ -0,0 +1,12 @@
+import 'package:auto_care_plus_app/app/modules/timeline/timeline_screen.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class TimelineModule extends Module {
+  @override
+  void routes(RouteManager r) {
+    r.child(Modular.initialRoute, child: (context) => const TimelineScreen());
+
+
+    super.routes(r);
+  }
+}
---
+++ lib/app/modules/timeline/timeline_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TimelineScreen extends StatefulWidget {
+  const TimelineScreen({super.key});
+
+  @override
+  State<TimelineScreen> createState() => _TimelineScreenState();
+}
+
+class _TimelineScreenState extends State<TimelineScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+}
