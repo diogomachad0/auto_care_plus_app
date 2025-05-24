0c520b98417f88c81f99d4a643ce739e9d98765e
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-24 20:44:21 -0300

    fix: tirando rotas desnecessárias

--- lib/app/modules/menu/menu_module.dart
+++ lib/app/modules/menu/menu_module.dart
@@ -1,7 +1,4 @@
-import 'package:auto_care_plus_app/app/modules/lembrete/lembrete_module.dart';
 import 'package:auto_care_plus_app/app/modules/menu/menu_screen.dart';
-import 'package:auto_care_plus_app/app/modules/veiculo/veiculo_module.dart';
-import 'package:auto_care_plus_app/app/shared/route/route.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class MenuModule extends Module {
@@ -9,8 +6,6 @@ class MenuModule extends Module {
   void routes(RouteManager r) {
     r.child(Modular.initialRoute, child: (context) => const MenuScreen());
 
-    r.module('/$veiculoRoute', module: VeiculoModule(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300));
-    r.module('/$lembreteRoute', module: LembreteModule(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300));
     super.routes(r);
   }
 }
