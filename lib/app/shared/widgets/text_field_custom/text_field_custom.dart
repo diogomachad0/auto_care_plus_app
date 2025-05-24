e9ef94a35b8450a7d88597e2fdfda75d3d70e785
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-24 20:41:30 -0300

    fix: ajustes no text field custom

--- lib/app/shared/widgets/text_field_custom/text_field_custom.dart
+++ lib/app/shared/widgets/text_field_custom/text_field_custom.dart
@@ -1,37 +1,47 @@
 import 'package:auto_care_plus_app/app/shared/mixin/theme_mixin.dart';
 import 'package:flutter/material.dart';
 
-class TextFieldCustom extends StatefulWidget{
+class TextFieldCustom extends StatefulWidget {
   final String label;
+  final VoidCallback? onTap;
+  final TextEditingController? controller;
 
   const TextFieldCustom({
     super.key,
     required this.label,
+    this.onTap,
+    this.controller,
   });
 
   @override
   State<TextFieldCustom> createState() => _TextFieldCustomState();
 }
 
-class _TextFieldCustomState extends State<TextFieldCustom> with ThemeMixin{
+class _TextFieldCustomState extends State<TextFieldCustom> with ThemeMixin {
+  late final TextEditingController _internalController;
+
   @override
-  Widget build(BuildContext context){
+  void initState() {
+    super.initState();
+    _internalController = widget.controller ?? TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return TextField(
+      controller: _internalController,
+      readOnly: widget.onTap != null,
+      onTap: widget.onTap,
       decoration: InputDecoration(
         labelText: widget.label,
-        labelStyle: textTheme.bodyMedium?.copyWith(
-          color: Colors.grey,
-        ),
+        labelStyle: textTheme.bodyMedium?.copyWith(color: Colors.grey),
         filled: true,
         fillColor: Colors.grey[200],
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(8),
           borderSide: BorderSide.none,
         ),
-        contentPadding: const EdgeInsets.symmetric(
-          horizontal: 8,
-          vertical: 8,
-        ),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
       ),
     );
   }
