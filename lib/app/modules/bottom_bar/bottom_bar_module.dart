1ae78b3d7fcdf1c23c1b7afbc72db7cd7cd45201
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-24 18:48:40 -0300

    fix: ajustes nas rotas s/ bottombar

--- lib/app/modules/bottom_bar/bottom_bar_module.dart
+++ lib/app/modules/bottom_bar/bottom_bar_module.dart
@@ -1,25 +1,29 @@
-
 import 'package:auto_care_plus_app/app/modules/bottom_bar/bottom_bar_screen.dart';
 import 'package:auto_care_plus_app/app/modules/home/home_module.dart';
+import 'package:auto_care_plus_app/app/modules/lembrete/lembrete_screen.dart';
 import 'package:auto_care_plus_app/app/modules/mapa/mapa_module.dart';
-import 'package:auto_care_plus_app/app/modules/menu/menu_module.dart';
+import 'package:auto_care_plus_app/app/modules/menu/menu_screen.dart';
 import 'package:auto_care_plus_app/app/modules/timeline/timeline_module.dart';
+import 'package:auto_care_plus_app/app/modules/veiculo/veiculo_screen.dart';
 import 'package:auto_care_plus_app/app/shared/route/route.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class BottomBarModule extends Module {
   @override
-  void binds(Injector i) {
-    super.binds(i);
-  }
-
-  @override
   void routes(RouteManager r) {
-    r.child(Modular.initialRoute, child: (context) => BottomBarScreen(disableNavigationMenu: r.args.data?['disable'] ?? false), children: [
-      ModuleRoute('/$homeRoute', module: HomeModule(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300)),
-      ModuleRoute('/$menuRoute', module: MenuModule(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300)),
-      ModuleRoute('/$timeLineRoute', module: TimelineModule(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300)),
-      ModuleRoute('/$mapaRoute', module: MapaModule(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300)),
-    ]);
+    r.child(
+      Modular.initialRoute,
+      child: (context) => BottomBarScreen(
+        disableNavigationMenu: r.args.data?['disable'] ?? false,
+      ),
+      children: [
+        ModuleRoute('/$homeRoute', module: HomeModule()),
+        ModuleRoute('/$mapaRoute', module: MapaModule()),
+        ModuleRoute('/$timeLineRoute', module: TimelineModule()),
+        ChildRoute('/$menuRoute', child: (context) => const MenuScreen()),
+      ],
+    );
+    r.child('/$veiculoRoute', child: (context) => const VeiculoScreen());
+    r.child('/$lembreteRoute', child: (context) => const LembreteScreen());
   }
 }
