df3d039cf8b064a53f006099656e14a55636717f
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-24 22:22:14 -0300

    feat: add nova rota no bottombar

--- lib/app/modules/bottom_bar/bottom_bar_module.dart
+++ lib/app/modules/bottom_bar/bottom_bar_module.dart
@@ -2,9 +2,9 @@ import 'package:auto_care_plus_app/app/modules/bottom_bar/bottom_bar_screen.dart
 import 'package:auto_care_plus_app/app/modules/contato/contato_screen.dart';
 import 'package:auto_care_plus_app/app/modules/home/home_module.dart';
 import 'package:auto_care_plus_app/app/modules/lembrete/lembrete_screen.dart';
-import 'package:auto_care_plus_app/app/modules/lembrete/submodules/adicionar_lembrete/adicionar_lembrete_widget.dart';
 import 'package:auto_care_plus_app/app/modules/mapa/mapa_module.dart';
 import 'package:auto_care_plus_app/app/modules/menu/menu_screen.dart';
+import 'package:auto_care_plus_app/app/modules/sobre/sobre_screen.dart';
 import 'package:auto_care_plus_app/app/modules/timeline/timeline_module.dart';
 import 'package:auto_care_plus_app/app/modules/veiculo/submodules/adicionar_veiculo/adicionar_veiculo_screen.dart';
 import 'package:auto_care_plus_app/app/modules/veiculo/veiculo_screen.dart';
@@ -30,5 +30,6 @@ class BottomBarModule extends Module {
     r.child('/$adicionarVeiculoRoute', child: (context) => const AdicionarVeiculoScreen());
     r.child('/$lembreteRoute', child: (context) => const LembreteScreen());
     r.child('/$contatoRoute', child: (context) => const ContatoScreen());
+    r.child('/$sobreRoute', child: (context) => const SobreScreen());
   }
 }
