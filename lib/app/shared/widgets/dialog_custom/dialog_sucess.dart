b5dad65cf8d58e8cce07d4a1489b6bd7a1f7267e
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-25 18:02:49 -0300

    feat: create dialog sucess

---
+++ lib/app/shared/widgets/dialog_custom/dialog_sucess.dart
@@ -0,0 +1,82 @@
+import 'package:auto_care_plus_app/app/shared/mixin/theme_mixin.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class DialogSucess extends StatefulWidget {
+  final String message;
+
+  const DialogSucess({super.key, required this.message});
+
+  @override
+  State<DialogSucess> createState() => _DialogSucessState();
+
+  static Future<void> show(BuildContext context, String message) async {
+    await showDialog(
+      context: context,
+      builder: (context) => DialogSucess(
+        message: message,
+      ),
+    );
+  }
+}
+
+class _DialogSucessState extends State<DialogSucess>
+    with ThemeMixin, SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    )..forward();
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.elasticOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shadowColor: Colors.black,
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ScaleTransition(
+            scale: _animation,
+            child: const Icon(
+              Icons.check_circle,
+              color: Colors.green,
+              size: 70,
+            ),
+          ),
+          const SizedBox(height: 32),
+          Text(
+            widget.message,
+            style: textTheme.bodyMedium,
+          ),
+        ],
+      ),
+      actions: [
+        FilledButton(
+          style: FilledButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          onPressed: () => Modular.to.pop(),
+          child: const Text('OK'),
+        )
+      ],
+    );
+  }
+}
