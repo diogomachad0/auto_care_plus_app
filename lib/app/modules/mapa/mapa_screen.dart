16409d8a94f176eaad5f60a4a13707a6bbe58087
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-29 19:17:46 -0300

    fix: correção layout mapa screen

--- lib/app/modules/mapa/mapa_screen.dart
+++ lib/app/modules/mapa/mapa_screen.dart
@@ -1,5 +1,8 @@
 import 'package:auto_care_plus_app/app/shared/mixin/theme_mixin.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'mapa_controller.dart';
 
 class MapaScreen extends StatefulWidget {
   const MapaScreen({super.key});
@@ -9,6 +12,14 @@ class MapaScreen extends StatefulWidget {
 }
 
 class _MapaScreenState extends State<MapaScreen> with ThemeMixin {
+  final MapaController controller = Modular.get<MapaController>();
+
+  @override
+  void initState() {
+    controller.mapaController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,8 +45,22 @@ class _MapaScreenState extends State<MapaScreen> with ThemeMixin {
           ),
         ),
       ),
-      body: Container(
-        color: Colors.green,
+      body: Center(
+        child: Observer(
+          builder: (context) {
+            String mensagem = controller.erro.isEmpty
+                ? 'Latitude: ${controller.lat} | Longitude: ${controller.long}'
+                : controller.erro;
+
+            return Text(
+              mensagem,
+              style: textTheme.bodyLarge?.copyWith(
+                color: colorScheme.primary,
+              ),
+              textAlign: TextAlign.center,
+            );
+          },
+        ),
       ),
     );
   }
