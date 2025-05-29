35a936325be06a376f6a3aaa09fd609d4a62a31a
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-29 19:17:30 -0300

    feat: add binds do módulo mapa

--- lib/app/modules/mapa/mapa_module.dart
+++ lib/app/modules/mapa/mapa_module.dart
@@ -1,7 +1,15 @@
+import 'package:auto_care_plus_app/app/modules/mapa/mapa_controller.dart';
 import 'package:auto_care_plus_app/app/modules/mapa/mapa_screen.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class MapaModule extends Module {
+  @override
+  void binds(Injector i) {
+    i.add<MapaController>(MapaController.new);
+
+    super.binds(i);
+  }
+
   @override
   void routes(RouteManager r) {
     r.child(Modular.initialRoute, child: (context) => const MapaScreen());
