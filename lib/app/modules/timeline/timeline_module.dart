da92c7ccf6dd78f00d45a75c2bd97026c5989b10
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-29 22:10:56 -0300

    fix: arrumando rotas

--- lib/app/modules/timeline/timeline_module.dart
+++ lib/app/modules/timeline/timeline_module.dart
@@ -1,10 +1,15 @@
+import 'package:auto_care_plus_app/app/modules/timeline/submodules/filtro_module.dart';
 import 'package:auto_care_plus_app/app/modules/timeline/timeline_screen.dart';
+import 'package:auto_care_plus_app/app/shared/route/route.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class TimelineModule extends Module {
   @override
   void routes(RouteManager r) {
     r.child(Modular.initialRoute, child: (context) => const TimelineScreen());
+
+    r.module('/$filtroRoute', module: FiltroModule(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300));
+
 
     super.routes(r);
   }
