4f14c19d8e2a73cb09be91f3ca961d8d407206ef
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-30 23:28:56 -0300

    fix: import modulo veiculo

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,8 +1,12 @@
 import 'package:auto_care_plus_app/app/app_controller.dart';
 import 'package:auto_care_plus_app/app/modules/authentication/login/login_module.dart';
+import 'package:auto_care_plus_app/app/modules/veiculo/veiculo_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
+  @override
+  List<Module> get imports => [VeiculoModule()];
+
   @override
   void binds(Injector i) {
     i.addSingleton<AppController>(AppController.new);
