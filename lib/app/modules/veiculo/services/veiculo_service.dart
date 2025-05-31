3a7f12e22162c4e74ecb4d303572124efded7175
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-30 22:31:47 -0300

    feat: add service

---
+++ lib/app/modules/veiculo/services/veiculo_service.dart
@@ -0,0 +1,9 @@
+import 'package:auto_care_plus_app/app/modules/veiculo/models/veiculo_model.dart';
+import 'package:auto_care_plus_app/app/modules/veiculo/repositories/veiculo_repository_interface.dart';
+import 'package:auto_care_plus_app/app/modules/veiculo/services/veiculo_service_interface.dart';
+import 'package:auto_care_plus_app/app/shared/services/service/service.dart';
+
+class VeiculoService extends BaseService<VeiculoModel, IVeiculoRepository>
+    implements IVeiculoService {
+  VeiculoService(super.repository);
+}
