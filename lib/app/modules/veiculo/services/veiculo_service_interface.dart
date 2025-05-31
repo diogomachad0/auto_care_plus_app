9b2cbb5aa68d252077ff6cc3bda07cfa86dc8436
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-30 22:31:40 -0300

    feat: add interface service

---
+++ lib/app/modules/veiculo/services/veiculo_service_interface.dart
@@ -0,0 +1,4 @@
+import 'package:auto_care_plus_app/app/modules/veiculo/models/veiculo_model.dart';
+import 'package:auto_care_plus_app/app/shared/services/service/service_interface.dart';
+
+abstract class IVeiculoService implements IService<VeiculoModel> {}
