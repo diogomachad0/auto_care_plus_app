618bc8a405cd27b7ff192bb905aa8903eaae518e
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-30 23:29:49 -0300

    feat: add binds module

--- lib/app/modules/veiculo/veiculo_module.dart
+++ lib/app/modules/veiculo/veiculo_module.dart
@@ -1,9 +1,22 @@
+import 'package:auto_care_plus_app/app/modules/veiculo/repositories/veiculo_repository.dart';
+import 'package:auto_care_plus_app/app/modules/veiculo/repositories/veiculo_repository_interface.dart';
+import 'package:auto_care_plus_app/app/modules/veiculo/services/veiculo_service.dart';
+import 'package:auto_care_plus_app/app/modules/veiculo/services/veiculo_service_interface.dart';
 import 'package:auto_care_plus_app/app/modules/veiculo/submodules/adicionar_veiculo/adicionar_veiculo_module.dart';
+import 'package:auto_care_plus_app/app/modules/veiculo/veiculo_controller.dart';
 import 'package:auto_care_plus_app/app/modules/veiculo/veiculo_screen.dart';
 import 'package:auto_care_plus_app/app/shared/route/route.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class VeiculoModule extends Module {
+  @override
+  void binds(Injector i) {
+    i.add<IVeiculoRepository>(VeiculoRepository.new);
+    i.add<IVeiculoService>(VeiculoService.new);
+    i.add<VeiculoController>(VeiculoController.new);
+
+    super.binds(i);
+  }
   @override
   void routes(RouteManager r) {
     r.child(Modular.initialRoute, child: (context) => const VeiculoScreen());
