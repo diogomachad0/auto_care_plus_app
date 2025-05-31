147a0aa0e16b0b864d745fa25f6f850b870f7fe6
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-30 21:31:47 -0300

    delete import

--- lib/app/modules/timeline/submodules/filtro_module.dart
+++ lib/app/modules/timeline/submodules/filtro_module.dart
@@ -1,4 +1,3 @@
-import 'package:auto_care_plus_app/app/modules/lembrete/submodules/adicionar_lembrete/adicionar_lembrete_widget.dart';
 import 'package:auto_care_plus_app/app/modules/timeline/submodules/filtro_widget.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
