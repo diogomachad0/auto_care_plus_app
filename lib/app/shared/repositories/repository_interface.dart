738bf7aab5bf3c6a6f37a0f4dabc193e378c1752
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-30 21:07:45 -0300

    feat: add métodos da interface

--- lib/app/shared/repositories/repository_interface.dart
+++ lib/app/shared/repositories/repository_interface.dart
@@ -2,3 +2,20 @@ import 'package:auto_care_plus_app/app/shared/models/base_model_interface.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:sqflite/sqflite.dart';
 
+abstract class IRepository<T extends IBaseModel> implements Disposable {
+  Future<List<T>> getAll({bool deleted = false, Transaction? txn});
+
+  Future<T?> getById(String id, {bool deleted = false});
+
+  Future<T?> getFirst({Transaction? txn});
+
+  Future<void> save(T entity);
+
+  Future<void> update(T entity);
+
+  Future delete(T entity);
+
+  Future<void> saveInTransaction(T entity, Transaction txn);
+
+  Future<void> updateInTransaction(T entity, Transaction txn);
+}
