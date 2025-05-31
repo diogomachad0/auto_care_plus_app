5bf58002438dbc10cb037102018a7e19b5ce56e9
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-30 21:31:54 -0300

    delete import

--- lib/app/modules/veiculo/submodules/adicionar_veiculo/adicionar_veiculo_module.dart
+++ lib/app/modules/veiculo/submodules/adicionar_veiculo/adicionar_veiculo_module.dart
@@ -1,5 +1,4 @@
 import 'package:auto_care_plus_app/app/modules/veiculo/submodules/adicionar_veiculo/adicionar_veiculo_screen.dart';
-import 'package:auto_care_plus_app/app/shared/route/route.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AdicionarVeiculoModule extends Module {
