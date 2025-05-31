ef0a4bac469f166e7ab6d6922a94126576e63f0d
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-30 22:30:32 -0300

    feat: create repository veiculo

---
+++ lib/app/modules/veiculo/repositories/veiculo_repository_interface.dart
@@ -0,0 +1,4 @@
+import 'package:auto_care_plus_app/app/modules/veiculo/models/veiculo_model.dart';
+import 'package:auto_care_plus_app/app/shared/repositories/repository_interface.dart';
+
+abstract class IVeiculoRepository implements IRepository<VeiculoModel> {}
