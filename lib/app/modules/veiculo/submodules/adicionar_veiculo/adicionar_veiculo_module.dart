228f2e00f6920108a8d3bed015505a725d2a0f2e
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-31 01:17:14 -0300

    feat: implementação da editar veiculo

--- lib/app/modules/veiculo/submodules/adicionar_veiculo/adicionar_veiculo_module.dart
+++ lib/app/modules/veiculo/submodules/adicionar_veiculo/adicionar_veiculo_module.dart
@@ -1,9 +1,24 @@
+import 'package:auto_care_plus_app/app/modules/veiculo/repositories/veiculo_repository.dart';
+import 'package:auto_care_plus_app/app/modules/veiculo/repositories/veiculo_repository_interface.dart';
+import 'package:auto_care_plus_app/app/modules/veiculo/services/veiculo_service.dart';
+import 'package:auto_care_plus_app/app/modules/veiculo/services/veiculo_service_interface.dart';
 import 'package:auto_care_plus_app/app/modules/veiculo/submodules/adicionar_veiculo/adicionar_veiculo_screen.dart';
+import 'package:auto_care_plus_app/app/modules/veiculo/veiculo_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AdicionarVeiculoModule extends Module {
   @override
   void routes(RouteManager r) {
+    @override
+    void binds(Injector i) {
+      i.add<IVeiculoRepository>(VeiculoRepository.new);
+      i.add<IVeiculoService>(VeiculoService.new);
+      i.add<VeiculoController>(VeiculoController.new);
+
+      super.binds(i);
+    }
+
+
     r.child(Modular.initialRoute, child: (context) => const AdicionarVeiculoScreen());
 
     super.routes(r);
