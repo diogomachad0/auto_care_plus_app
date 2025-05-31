72970092b897576dd498d5823639d2734c22f589
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-30 22:30:44 -0300

    feat: criado repository do veiculo

---
+++ lib/app/modules/veiculo/repositories/veiculo_repository.dart
@@ -0,0 +1,67 @@
+import 'package:auto_care_plus_app/app/modules/veiculo/models/veiculo_model.dart';
+import 'package:auto_care_plus_app/app/modules/veiculo/repositories/veiculo_repository_interface.dart';
+import 'package:auto_care_plus_app/app/shared/repositories/base_repository.dart';
+import 'package:auto_care_plus_app/app/shared/repositories/table_repository_local.dart';
+import 'package:sqflite/sqflite.dart';
+
+class VeiculoRepository extends TableRepositoryLocal<VeiculoModel>
+    implements IVeiculoRepository {
+  @override
+  String get getTableName => 'veiculo';
+
+  @override
+  void create(Batch batch) {
+    batch.execute('''
+      CREATE TABLE $getTableName (
+        $getIdColumnName TEXT PRIMARY KEY,
+        ${BaseRepository.dataHoraCriado} TEXT NOT NULL,
+        ${BaseRepository.dataHoraDeletado} TEXT,
+        ${BaseRepository.dataHoraUltimaAlteracao} TEXT,
+        modelo TEXT NOT NULL,
+        marca TEXT NOT NULL,
+        placa TEXT NOT NULL,
+        ano TEXT NOT NULL,
+        quilometragem TEXT NOT NULL,
+        tipo_combustivel TEXT NOT NULL,
+        observacoes TEXT
+      );
+    ''');
+  }
+
+  @override
+  VeiculoModel fromMap(Map<String, dynamic> e) => VeiculoModel(
+        base: baseFromMap(e),
+        modelo: e['modelo'],
+        marca: e['marca'],
+        placa: e['placa'],
+        ano: e['ano'],
+        quilometragem: e['quilometragem'],
+        tipoCombustivel: e['tipo_combustivel'],
+        observacoes: e['observacoes'] ?? '',
+      );
+
+  @override
+  Map<String, dynamic> toMap(VeiculoModel entity) => {
+        getIdColumnName: entity.base.id,
+        BaseRepository.dataHoraCriado:
+            entity.base.dataHoraCriado?.toIso8601String() ??
+                DateTime.now().toIso8601String(),
+        BaseRepository.dataHoraDeletado:
+            entity.base.dataHoraDeletado?.toIso8601String(),
+        BaseRepository.dataHoraUltimaAlteracao:
+            entity.base.dataHoraUltimaAlteracao?.toIso8601String(),
+        'modelo': entity.modelo,
+        'marca': entity.marca,
+        'placa': entity.placa,
+        'ano': entity.ano,
+        'quilometragem': entity.quilometragem,
+        'tipo_combustivel': entity.tipoCombustivel,
+        'observacoes': entity.observacoes,
+      };
+
+  @override
+  void validate(VeiculoModel entity) {}
+
+  @override
+  void dispose() {}
+}
