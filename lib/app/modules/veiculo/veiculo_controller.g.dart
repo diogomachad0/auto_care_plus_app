e559773bb1915873b1fe2131eed8ae2af8cedc70
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-05-31 17:28:28 -0300

    fix

--- lib/app/modules/veiculo/veiculo_controller.g.dart
+++ lib/app/modules/veiculo/veiculo_controller.g.dart
@@ -9,6 +9,14 @@ part of 'veiculo_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$VeiculoController on _VeiculoControllerBase, Store {
+  Computed<List<VeiculoStore>>? _$veiculosFiltradosComputed;
+
+  @override
+  List<VeiculoStore> get veiculosFiltrados => (_$veiculosFiltradosComputed ??=
+          Computed<List<VeiculoStore>>(() => super.veiculosFiltrados,
+              name: '_VeiculoControllerBase.veiculosFiltrados'))
+      .value;
+
   late final _$veiculoAtom =
       Atom(name: '_VeiculoControllerBase.veiculo', context: context);
 
@@ -25,10 +33,45 @@ mixin _$VeiculoController on _VeiculoControllerBase, Store {
     });
   }
 
+  late final _$veiculosAtom =
+      Atom(name: '_VeiculoControllerBase.veiculos', context: context);
+
+  @override
+  ObservableList<VeiculoStore> get veiculos {
+    _$veiculosAtom.reportRead();
+    return super.veiculos;
+  }
+
+  @override
+  set veiculos(ObservableList<VeiculoStore> value) {
+    _$veiculosAtom.reportWrite(value, super.veiculos, () {
+      super.veiculos = value;
+    });
+  }
+
+  late final _$searchTextAtom =
+      Atom(name: '_VeiculoControllerBase.searchText', context: context);
+
+  @override
+  String get searchText {
+    _$searchTextAtom.reportRead();
+    return super.searchText;
+  }
+
+  @override
+  set searchText(String value) {
+    _$searchTextAtom.reportWrite(value, super.searchText, () {
+      super.searchText = value;
+    });
+  }
+
   @override
   String toString() {
     return '''
-veiculo: ${veiculo}
+veiculo: ${veiculo},
+veiculos: ${veiculos},
+searchText: ${searchText},
+veiculosFiltrados: ${veiculosFiltrados}
     ''';
   }
 }
