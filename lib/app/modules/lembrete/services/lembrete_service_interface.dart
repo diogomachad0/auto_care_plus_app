d7b25e6f5a69d6e84c1e08efcd6acfe86b8d73c7
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-06-01 14:40:10 -0300

    feat: add interface service

---
+++ lib/app/modules/lembrete/services/lembrete_service_interface.dart
@@ -0,0 +1,4 @@
+import 'package:auto_care_plus_app/app/modules/lembrete/models/lembrete_model.dart';
+import 'package:auto_care_plus_app/app/shared/services/service/service_interface.dart';
+
+abstract class ILembreteService implements IService<LembreteModel> {}
