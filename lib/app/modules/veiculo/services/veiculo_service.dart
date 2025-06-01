ad6f604d24d5f030f3b39e7cb987ee208c08d6c4
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-06-01 14:40:37 -0300

    fix

--- lib/app/modules/veiculo/services/veiculo_service.dart
+++ lib/app/modules/veiculo/services/veiculo_service.dart
@@ -3,7 +3,6 @@ import 'package:auto_care_plus_app/app/modules/veiculo/repositories/veiculo_repo
 import 'package:auto_care_plus_app/app/modules/veiculo/services/veiculo_service_interface.dart';
 import 'package:auto_care_plus_app/app/shared/services/service/service.dart';
 
-class VeiculoService extends BaseService<VeiculoModel, IVeiculoRepository>
-    implements IVeiculoService {
+class VeiculoService extends BaseService<VeiculoModel, IVeiculoRepository> implements IVeiculoService {
   VeiculoService(super.repository);
 }
