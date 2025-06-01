79ac7b03050ec54b71bd73c2b967fab6962e2b9f
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-06-01 16:44:47 -0300

    feat: add modulo do lembrete

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,11 +1,12 @@
 import 'package:auto_care_plus_app/app/app_controller.dart';
 import 'package:auto_care_plus_app/app/modules/authentication/login/login_module.dart';
+import 'package:auto_care_plus_app/app/modules/lembrete/lembrete_module.dart';
 import 'package:auto_care_plus_app/app/modules/veiculo/veiculo_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
   @override
-  List<Module> get imports => [VeiculoModule()];
+  List<Module> get imports => [VeiculoModule(), LembreteModule()];
 
   @override
   void binds(Injector i) {
