5d3d47a6c06d3743053f93f6a8c4028ec67c0843
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-06-01 16:45:33 -0300

    feat: add injeção p/ o modulo

--- lib/app/modules/lembrete/lembrete_module.dart
+++ lib/app/modules/lembrete/lembrete_module.dart
@@ -1,13 +1,26 @@
+import 'package:auto_care_plus_app/app/modules/lembrete/lembrete_controller.dart';
 import 'package:auto_care_plus_app/app/modules/lembrete/lembrete_screen.dart';
+import 'package:auto_care_plus_app/app/modules/lembrete/repositories/lembrete_repository.dart';
+import 'package:auto_care_plus_app/app/modules/lembrete/repositories/lembrete_repository_interface.dart';
+import 'package:auto_care_plus_app/app/modules/lembrete/services/lembrete_service.dart';
+import 'package:auto_care_plus_app/app/modules/lembrete/services/lembrete_service_interface.dart';
 import 'package:auto_care_plus_app/app/modules/lembrete/submodules/adicionar_lembrete/adicionar_lembrete_module.dart';
 import 'package:auto_care_plus_app/app/shared/route/route.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class LembreteModule extends Module {
   @override
+  void binds(Injector i) {
+    i.add<ILembreteRepository>(LembreteRepository.new);
+    i.add<ILembreteService>(LembreteService.new);
+    i.add<LembreteController>(LembreteController.new);
+
+    super.binds(i);
+  }
+
+  @override
   void routes(RouteManager r) {
     r.child(Modular.initialRoute, child: (context) => const LembreteScreen());
-
 
     r.module('/$adicioanarLembreteRoute', module: AdicionarLembreteModule(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300));
     super.routes(r);
