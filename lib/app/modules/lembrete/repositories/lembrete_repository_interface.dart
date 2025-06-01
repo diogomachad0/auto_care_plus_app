2b5319c9a3d8498bdc203ec7a0a72efcf3ab956f
Diogo Machado <diogomachado0801@rede.ulbra.br>
2025-06-01 14:39:25 -0300

    feat: add interface repository

---
+++ lib/app/modules/lembrete/repositories/lembrete_repository_interface.dart
@@ -0,0 +1,4 @@
+import 'package:auto_care_plus_app/app/modules/lembrete/models/lembrete_model.dart';
+import 'package:auto_care_plus_app/app/shared/repositories/repository_interface.dart';
+
+abstract class ILembreteRepository implements IRepository<LembreteModel> {}
