37859f66c97883259a49a6965e2d4c13dee8cd6e
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-04 19:50:27 -0300

    feat: add novas funcionalidades

--- lib/app/modules/lembrete/lembrete_controller.dart
+++ lib/app/modules/lembrete/lembrete_controller.dart
@@ -1,5 +1,6 @@
 import 'package:auto_care_plus_app/app/modules/lembrete/services/lembrete_service_interface.dart';
 import 'package:auto_care_plus_app/app/modules/lembrete/store/lembrete_store.dart';
+import 'package:auto_care_plus_app/app/shared/services/notification_service/notification_service.dart';
 import 'package:mobx/mobx.dart';
 
 part 'lembrete_controller.g.dart';
@@ -8,9 +9,7 @@ class LembreteController = _LembreteControllerBase with _$LembreteController;
 
 abstract class _LembreteControllerBase with Store {
   final ILembreteService _service;
-
-  @observable
-  String searchText = '';
+  final NotificationService _notificationService = NotificationService();
 
   @observable
   LembreteStore lembrete = LembreteStoreFactory.novo();
@@ -32,12 +31,33 @@ abstract class _LembreteControllerBase with Store {
 
   @action
   Future<void> save() async {
-    await _service.saveOrUpdate(lembrete.toModel());
+    final model = lembrete.toModel();
+    await _service.saveOrUpdate(model);
+
+    // Se o lembrete deve notificar, criar a notificação
+    if (lembrete.notificar) {
+      await _showLembreteNotification(model);
+    }
 
     await load();
   }
 
+  Future<void> _showLembreteNotification(dynamic lembreteModel) async {
+    final int notificationId = '${lembreteModel.titulo}${lembreteModel.data.millisecondsSinceEpoch}'.hashCode;
+
+    await _notificationService.showLembreteNotification(
+      id: notificationId,
+      titulo: lembreteModel.titulo,
+      data: lembreteModel.data,
+    );
+  }
+
   Future<void> delete(LembreteStore lembrete) async {
+    if (lembrete.notificar) {
+      final int notificationId = '${lembrete.titulo}${lembrete.data.millisecondsSinceEpoch}'.hashCode;
+      await _notificationService.cancelNotification(notificationId);
+    }
+
     await _service.delete(lembrete.toModel());
     lembretes.remove(lembrete);
   }
