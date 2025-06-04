caa6fcd9b1cae60f34289ccaec31fa13e4169a31
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-04 20:46:34 -0300

    feat: add interface service

---
+++ lib/app/modules/atividade/services/atividade_service_interface.dart
@@ -0,0 +1,4 @@
+import 'package:auto_care_plus_app/app/modules/atividade/models/atividade_model.dart';
+import 'package:auto_care_plus_app/app/shared/services/service/service_interface.dart';
+
+abstract class IAtividadeService implements IService<AtividadeModel> {}
