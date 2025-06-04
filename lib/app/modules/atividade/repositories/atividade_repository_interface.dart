d17bea37280efcdc76a18cc5b0cbefd4e7e70923
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-04 20:42:26 -0300

    feat: add interface repository

---
+++ lib/app/modules/atividade/repositories/atividade_repository_interface.dart
@@ -0,0 +1,4 @@
+import 'package:auto_care_plus_app/app/modules/atividade/models/atividade_model.dart';
+import 'package:auto_care_plus_app/app/shared/repositories/repository_interface.dart';
+
+abstract class IAtividadeRepository implements IRepository<AtividadeModel> {}
