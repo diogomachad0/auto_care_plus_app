a9455b685bd9fc019b2afeba27dd13ad6862d00c
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-04 19:50:34 -0300

    fix

--- lib/app/modules/lembrete/lembrete_controller.g.dart
+++ lib/app/modules/lembrete/lembrete_controller.g.dart
@@ -9,22 +9,6 @@ part of 'lembrete_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$LembreteController on _LembreteControllerBase, Store {
-  late final _$searchTextAtom =
-      Atom(name: '_LembreteControllerBase.searchText', context: context);
-
-  @override
-  String get searchText {
-    _$searchTextAtom.reportRead();
-    return super.searchText;
-  }
-
-  @override
-  set searchText(String value) {
-    _$searchTextAtom.reportWrite(value, super.searchText, () {
-      super.searchText = value;
-    });
-  }
-
   late final _$lembreteAtom =
       Atom(name: '_LembreteControllerBase.lembrete', context: context);
 
@@ -82,7 +66,6 @@ mixin _$LembreteController on _LembreteControllerBase, Store {
   @override
   String toString() {
     return '''
-searchText: ${searchText},
 lembrete: ${lembrete},
 lembretes: ${lembretes}
     ''';
