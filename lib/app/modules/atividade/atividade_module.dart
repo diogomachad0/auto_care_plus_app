7e7f8aae03cdb1d877c4acde79bc2dca0aca8623
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-04 23:55:46 -0300

    feat: add binds

--- lib/app/modules/atividade/atividade_module.dart
+++ lib/app/modules/atividade/atividade_module.dart
@@ -1,7 +1,22 @@
+import 'package:auto_care_plus_app/app/modules/atividade/atividade_controller.dart';
 import 'package:auto_care_plus_app/app/modules/atividade/atividade_screen.dart';
+import 'package:auto_care_plus_app/app/modules/atividade/repositories/atividade_repository.dart';
+import 'package:auto_care_plus_app/app/modules/atividade/repositories/atividade_repository_interface.dart';
+import 'package:auto_care_plus_app/app/modules/atividade/services/atividade_service.dart';
+import 'package:auto_care_plus_app/app/modules/atividade/services/atividade_service_interface.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AtividadeModule extends Module {
+  @override
+  void binds(Injector i) {
+    i.add<IAtividadeRepository>(AtividadeRepository.new);
+    i.add<IAtividadeService>(AtividadeService.new);
+    i.add<AtividadeController>(AtividadeController.new);
+
+    super.binds(i);
+  }
+
+
   @override
   void routes(RouteManager r) {
     r.child(Modular.initialRoute, child: (context) => const AtividadeScreen());
