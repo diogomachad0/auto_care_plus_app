4a53ad5e0c066a11ac9875eee275c4c73ee095bd
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-04 23:56:58 -0300

    feat: add binds p/ timeline

--- lib/app/modules/timeline/timeline_module.dart
+++ lib/app/modules/timeline/timeline_module.dart
@@ -1,9 +1,24 @@
+import 'package:auto_care_plus_app/app/modules/atividade/atividade_controller.dart';
+import 'package:auto_care_plus_app/app/modules/atividade/repositories/atividade_repository.dart';
+import 'package:auto_care_plus_app/app/modules/atividade/repositories/atividade_repository_interface.dart';
+import 'package:auto_care_plus_app/app/modules/atividade/services/atividade_service.dart';
+import 'package:auto_care_plus_app/app/modules/atividade/services/atividade_service_interface.dart';
 import 'package:auto_care_plus_app/app/modules/timeline/submodules/filtro_module.dart';
 import 'package:auto_care_plus_app/app/modules/timeline/timeline_screen.dart';
 import 'package:auto_care_plus_app/app/shared/route/route.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class TimelineModule extends Module {
+  @override
+  void binds(Injector i) {
+    i.add<IAtividadeRepository>(AtividadeRepository.new);
+    i.add<IAtividadeService>(AtividadeService.new);
+    i.add<AtividadeController>(AtividadeController.new);
+
+    super.binds(i);
+  }
+
+
   @override
   void routes(RouteManager r) {
     r.child(Modular.initialRoute, child: (context) => const TimelineScreen());
