47b6d726087f1f6540d19197f1737d41a0b9bae3
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-08 01:12:57 -0300

    feat: add binds

--- lib/app/modules/mapa/mapa_module.dart
+++ lib/app/modules/mapa/mapa_module.dart
@@ -1,3 +1,8 @@
+import 'package:auto_care_plus_app/app/modules/atividade/atividade_controller.dart';
+import 'package:auto_care_plus_app/app/modules/atividade/repositories/atividade_repository.dart';
+import 'package:auto_care_plus_app/app/modules/atividade/repositories/atividade_repository_interface.dart';
+import 'package:auto_care_plus_app/app/modules/atividade/services/atividade_service.dart';
+import 'package:auto_care_plus_app/app/modules/atividade/services/atividade_service_interface.dart';
 import 'package:auto_care_plus_app/app/modules/mapa/mapa_controller.dart';
 import 'package:auto_care_plus_app/app/modules/mapa/mapa_screen.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -6,6 +11,9 @@ class MapaModule extends Module {
   @override
   void binds(Injector i) {
     i.add<MapaController>(MapaController.new);
+    i.add<AtividadeController>(AtividadeController.new);
+    i.add<IAtividadeService>(AtividadeService.new);
+    i.add<IAtividadeRepository>(AtividadeRepository.new);
 
     super.binds(i);
   }
