bbc17e217d61f088e1c56252ffafd28a2fe82394
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-08 01:12:46 -0300

    fix: implementação atividade

--- lib/app/modules/mapa/mapa_controller.dart
+++ lib/app/modules/mapa/mapa_controller.dart
@@ -1,14 +1,21 @@
+import 'package:auto_care_plus_app/app/modules/atividade/atividade_controller.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:mobx/mobx.dart';
-import 'package:geolocator/geolocator.dart';
 
 part 'mapa_controller.g.dart';
 
 class MapaController = _MapaControllerBase with _$MapaController;
 
 abstract class _MapaControllerBase with Store {
+  final AtividadeController _atividadeController = Modular.get<AtividadeController>();
+
   @observable
   LatLng? myPosition;
+
+  @computed
+  get atividadesComLocalizacao => _atividadeController.atividadesComCoordenadas;
 
   @action
   Future<void> mapaController() async {
@@ -16,6 +23,8 @@ abstract class _MapaControllerBase with Store {
       Position position = await _determinePosition();
       myPosition = LatLng(position.latitude, position.longitude);
       print('Localização atual: $myPosition');
+
+      await _atividadeController.load();
     } catch (e) {
       print('Erro ao obter localização: $e');
     }
