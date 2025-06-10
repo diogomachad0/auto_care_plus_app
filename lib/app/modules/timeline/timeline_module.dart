982c69a3f0bde5129367533f7fb88dfc73af6271
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-09 21:13:46 -0300

    feat: importado modulo veiculo

--- lib/app/modules/timeline/timeline_module.dart
+++ lib/app/modules/timeline/timeline_module.dart
@@ -5,10 +5,14 @@ import 'package:auto_care_plus_app/app/modules/atividade/services/atividade_serv
 import 'package:auto_care_plus_app/app/modules/atividade/services/atividade_service_interface.dart';
 import 'package:auto_care_plus_app/app/modules/timeline/submodules/filtro_module.dart';
 import 'package:auto_care_plus_app/app/modules/timeline/timeline_screen.dart';
+import 'package:auto_care_plus_app/app/modules/veiculo/veiculo_module.dart';
 import 'package:auto_care_plus_app/app/shared/route/route.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class TimelineModule extends Module {
+  @override
+  List<Module> get imports => [VeiculoModule()];
+
   @override
   void binds(Injector i) {
     i.add<IAtividadeRepository>(AtividadeRepository.new);
@@ -18,13 +22,11 @@ class TimelineModule extends Module {
     super.binds(i);
   }
 
-
   @override
   void routes(RouteManager r) {
     r.child(Modular.initialRoute, child: (context) => const TimelineScreen());
 
     r.module('/$filtroRoute', module: FiltroModule(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300));
-
 
     super.routes(r);
   }
