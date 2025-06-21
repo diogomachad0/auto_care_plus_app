a72cf4903a38894c57c8630fa57698a41941d7e9
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-21 17:51:06 -0300

    feat: add interface

---
+++ lib/app/modules/usuario/services/usuario_services_interface.dart
@@ -0,0 +1,14 @@
+import 'package:auto_care_plus_app/app/modules/usuario/models/usuario_model.dart';
+import 'package:auto_care_plus_app/app/shared/services/service/service_interface.dart';
+
+abstract class IUsuarioService implements IService<UsuarioModel> {
+  Future<UsuarioModel?> getByEmail(String email);
+
+  Future<UsuarioModel?> getCurrentUser();
+
+  Future<void> updateFirebaseProfile(UsuarioModel usuario);
+
+  Future<void> updatePassword(String currentPassword, String newPassword);
+
+  Future<void> deleteAccount();
+}
