7e35227123184d2944e22434849b4ffa72321246
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-21 17:46:22 -0300

    feat: add binds usuario

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,12 +1,17 @@
 import 'package:auto_care_plus_app/app/app_controller.dart';
 import 'package:auto_care_plus_app/app/modules/lembrete/lembrete_module.dart';
-import 'package:auto_care_plus_app/app/modules/veiculo/veiculo_module.dart';
 import 'package:auto_care_plus_app/app/modules/splash/splash_module.dart';
+import 'package:auto_care_plus_app/app/modules/usuario/usuario_module.dart';
+import 'package:auto_care_plus_app/app/modules/veiculo/veiculo_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
   @override
-  List<Module> get imports => [VeiculoModule(), LembreteModule()];
+  List<Module> get imports => [
+        VeiculoModule(),
+        LembreteModule(),
+        UsuarioModule(),
+      ];
 
   @override
   void binds(Injector i) {
