bbb6aeb70e5eba8d29a87c04f7fcbe7e1024bf0c
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-21 17:47:37 -0300

    feat: add binds

--- lib/app/modules/conta/conta_module.dart
+++ lib/app/modules/conta/conta_module.dart
@@ -1,7 +1,19 @@
+import 'package:auto_care_plus_app/app/modules/conta/conta_controller.dart';
 import 'package:auto_care_plus_app/app/modules/conta/conta_screen.dart';
+import 'package:auto_care_plus_app/app/modules/usuario/usuario_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class ContaModule extends Module {
+  @override
+  List<Module> get imports => [UsuarioModule()];
+
+  @override
+  void binds(Injector i) {
+    i.add<ContaController>(ContaController.new);
+
+    super.binds(i);
+  }
+
   @override
   void routes(RouteManager r) {
     r.child(Modular.initialRoute, child: (context) => const ContaScreen());
