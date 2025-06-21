a6be0d7e9d2db530edbeee78134b3a1991742798
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-21 17:50:11 -0300

    feat: add interface

---
+++ lib/app/modules/usuario/repositories/usuario_repository_interface.dart
@@ -0,0 +1,4 @@
+import 'package:auto_care_plus_app/app/modules/usuario/models/usuario_model.dart';
+import 'package:auto_care_plus_app/app/shared/repositories/repository_interface.dart';
+
+abstract class IUsuarioRepository implements IRepository<UsuarioModel> {}
