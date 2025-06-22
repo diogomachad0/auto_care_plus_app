93bd51de5c134bae4b852b9c35e52ac986f00ea7
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-22 20:32:20 -0300

    feat: add novo import da contato

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,4 +1,5 @@
 import 'package:auto_care_plus_app/app/app_controller.dart';
+import 'package:auto_care_plus_app/app/modules/contato/contato_module.dart';
 import 'package:auto_care_plus_app/app/modules/lembrete/lembrete_module.dart';
 import 'package:auto_care_plus_app/app/modules/splash/splash_module.dart';
 import 'package:auto_care_plus_app/app/modules/usuario/usuario_module.dart';
@@ -7,11 +8,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
   @override
-  List<Module> get imports => [
-        VeiculoModule(),
-        LembreteModule(),
-        UsuarioModule(),
-      ];
+  List<Module> get imports => [VeiculoModule(), LembreteModule(), UsuarioModule(), ContatoModule()];
 
   @override
   void binds(Injector i) {
