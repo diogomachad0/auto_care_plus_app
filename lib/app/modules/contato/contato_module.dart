1f8ffd5e657802ee56b26621532b9c0d0f8f74f2
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-22 20:33:04 -0300

    feat: add binds

--- lib/app/modules/contato/contato_module.dart
+++ lib/app/modules/contato/contato_module.dart
@@ -1,11 +1,17 @@
+import 'package:auto_care_plus_app/app/modules/contato/contato_controller.dart';
 import 'package:auto_care_plus_app/app/modules/contato/contato_screen.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class ContatoModule extends Module {
   @override
+  void binds(Injector i) {
+    i.add<ContatoController>(ContatoController.new);
+    super.binds(i);
+  }
+
+  @override
   void routes(RouteManager r) {
     r.child(Modular.initialRoute, child: (context) => const ContatoScreen());
-
     super.routes(r);
   }
 }
