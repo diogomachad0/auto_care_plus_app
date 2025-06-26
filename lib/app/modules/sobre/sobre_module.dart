98be7d1e733cf178b5cc1dfa314b916d940da203
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-25 21:02:08 -0300

    feat: add efeito fadein

--- lib/app/modules/sobre/sobre_module.dart
+++ lib/app/modules/sobre/sobre_module.dart
@@ -4,7 +4,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 class SobreModule extends Module {
   @override
   void routes(RouteManager r) {
-    r.child(Modular.initialRoute, child: (context) => const SobreScreen());
+    r.child(Modular.initialRoute, child: (context) => const SobreScreen(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300));
 
     super.routes(r);
   }
