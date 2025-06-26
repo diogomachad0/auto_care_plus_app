9c618632e29d7bcf8a475fc2c4ff3a8d7f7ce82b
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-25 21:00:55 -0300

    fix: ajustes em melhorias das rotas

--- lib/app/modules/bottom_bar/bottom_bar_module.dart
+++ lib/app/modules/bottom_bar/bottom_bar_module.dart
@@ -5,7 +5,7 @@ import 'package:auto_care_plus_app/app/modules/contato/contato_screen.dart';
 import 'package:auto_care_plus_app/app/modules/home/home_module.dart';
 import 'package:auto_care_plus_app/app/modules/lembrete/lembrete_screen.dart';
 import 'package:auto_care_plus_app/app/modules/mapa/mapa_module.dart';
-import 'package:auto_care_plus_app/app/modules/menu/menu_module.dart';
+import 'package:auto_care_plus_app/app/modules/menu/menu_screen.dart';
 import 'package:auto_care_plus_app/app/modules/sobre/sobre_screen.dart';
 import 'package:auto_care_plus_app/app/modules/timeline/timeline_module.dart';
 import 'package:auto_care_plus_app/app/modules/veiculo/submodules/adicionar_veiculo/adicionar_veiculo_screen.dart';
@@ -19,7 +19,6 @@ class BottomBarModule extends Module {
   @override
   void binds(Injector i) {
     i.add<AuthService>(AuthService.new);
-
     super.binds(i);
   }
 
@@ -27,23 +26,22 @@ class BottomBarModule extends Module {
   void routes(RouteManager r) {
     r.child(
       Modular.initialRoute,
-      child: (context) => BottomBarScreen(
-        disableNavigationMenu: r.args.data?['disable'] ?? false,
-      ),
+      child: (context) => const BottomBarScreen(),
       children: [
-        ModuleRoute('/$homeRoute', module: HomeModule()),
-        ModuleRoute('/$mapaRoute', module: MapaModule()),
-        ModuleRoute('/$timeLineRoute', module: TimelineModule()),
-        ModuleRoute('/$atividadeRoute', module: AtividadeModule()),
-        ModuleRoute('/$menuRoute', module: MenuModule()),
+        ModuleRoute('/$homeRoute', module: HomeModule(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300)),
+        ModuleRoute('/$mapaRoute', module: MapaModule(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300)),
+        ModuleRoute('/$timeLineRoute', module: TimelineModule(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300)),
+        ModuleRoute('/$atividadeRoute', module: AtividadeModule(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300)),
+        ChildRoute('/$menuRoute', child: (context) => const MenuScreen(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300)),
       ],
     );
-    r.child('/$veiculoRoute', child: (context) => const VeiculoScreen());
-    r.child('/$adicionarVeiculoRoute', child: (context) => const AdicionarVeiculoScreen());
-    r.child('/$editarVeiculoRoute', child: (context) => EditarVeiculoScreen());
-    r.child('/$lembreteRoute', child: (context) => const LembreteScreen());
-    r.child('/$contatoRoute', child: (context) => const ContatoScreen());
-    r.child('/$sobreRoute', child: (context) => const SobreScreen());
-    r.child('/$contaRoute', child: (context) => const ContaScreen());
+
+    r.child('/$veiculoRoute', child: (context) => const VeiculoScreen(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300));
+    r.child('/$adicionarVeiculoRoute', child: (context) => const AdicionarVeiculoScreen(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300));
+    r.child('/$editarVeiculoRoute', child: (context) => const EditarVeiculoScreen(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300));
+    r.child('/$lembreteRoute', child: (context) => const LembreteScreen(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300));
+    r.child('/$contatoRoute', child: (context) => const ContatoScreen(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300));
+    r.child('/$sobreRoute', child: (context) => const SobreScreen(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300));
+    r.child('/$contaRoute', child: (context) => const ContaScreen(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300));
   }
 }
