82504f5969d0d63ca71ee0906270542c0e299aeb
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-25 21:01:47 -0300

    feat: add efeito fadein

--- lib/app/modules/menu/menu_module.dart
+++ lib/app/modules/menu/menu_module.dart
@@ -4,7 +4,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 class MenuModule extends Module {
   @override
   void routes(RouteManager r) {
-    r.child(Modular.initialRoute, child: (context) => const MenuScreen());
+    r.child(Modular.initialRoute, child: (context) => const MenuScreen(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300));
 
     super.routes(r);
   }
