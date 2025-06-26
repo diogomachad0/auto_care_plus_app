ad4cc54fc6ce3378227c82a1b9ef69af5cdba7a1
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-25 21:00:28 -0300

    feat: add efeito fadein

--- lib/app/modules/authentication/recuperar_senha/recuperar_senha_module.dart
+++ lib/app/modules/authentication/recuperar_senha/recuperar_senha_module.dart
@@ -4,7 +4,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 class RecuperarSenhaModule extends Module {
   @override
   void routes(RouteManager r) {
-    r.child(Modular.initialRoute, child: (context) => const RecuperarSenhaScreen());
+    r.child(Modular.initialRoute, child: (context) => const RecuperarSenhaScreen(), transition: TransitionType.fadeIn, duration: const Duration(milliseconds: 300));
 
     super.routes(r);
   }
