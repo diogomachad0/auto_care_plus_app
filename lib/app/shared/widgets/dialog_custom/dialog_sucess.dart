fe9d40ddabbb9e11eb2fba56f1c01bbefa579579
Diogo Machado <101813097+diogomachad0@users.noreply.github.com>
2025-06-26 23:15:14 -0300

    feat: implementação autoclose

--- lib/app/shared/widgets/dialog_custom/dialog_sucess.dart
+++ lib/app/shared/widgets/dialog_custom/dialog_sucess.dart
@@ -6,18 +6,24 @@ import 'package:flutter_modular/flutter_modular.dart';
 
 class DialogSucess extends StatefulWidget {
   final String message;
+  final bool autoClose;
 
-  const DialogSucess(this.message, {super.key});
+  const DialogSucess(this.message, {super.key, this.autoClose = false});
 
   @override
   State<DialogSucess> createState() => _DialogSucessState();
 
-  static Future<void> show(BuildContext context, String message) async {
+  static Future<void> show(
+    BuildContext context,
+    String message, {
+    bool autoClose = false,
+  }) async {
     await showDialog(
       context: context,
+      barrierDismissible: false,
       builder: (context) => BackdropFilter(
         filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
-        child: DialogSucess(message),
+        child: DialogSucess(message, autoClose: autoClose),
       ),
     );
   }
@@ -30,14 +36,30 @@ class _DialogSucessState extends State<DialogSucess> with ThemeMixin, SingleTick
   @override
   void initState() {
     super.initState();
+
     _controller = AnimationController(
       duration: const Duration(seconds: 1),
       vsync: this,
     )..forward();
+
     _animation = CurvedAnimation(
       parent: _controller,
       curve: Curves.elasticOut,
     );
+
+    if (widget.autoClose) {
+      Future.delayed(const Duration(seconds: 3), () {
+        if (mounted) {
+          Modular.to.pop();
+        }
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -60,7 +82,7 @@ class _DialogSucessState extends State<DialogSucess> with ThemeMixin, SingleTick
           Text(
             'Oba!',
             style: textTheme.titleLarge,
-          )
+          ),
         ],
       ),
       content: Text(
